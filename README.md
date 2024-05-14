d201dadfb7cae3f0315d434b157844c29f2048ba
MrLaki5 <mimar1207@gmail.com>
2024-05-14 21:58:52 +0200

    fix: redis failed connection; update: frontend

--- README.md
+++ README.md
@@ -1,22 +1,36 @@
 # QR-AI-generator
 Transform ordinary QR codes into engaging visual experiences with AI technology.
 
+## Overview
+Our service streamlines the process of creating visually enhanced QR codes through a user-friendly three-screen interface.
+<div style="display: flex; justify-content: space-between;">
+  <img src="images/step1.png" style="width: 32%;"/>
+  <img src="images/step2.png" style="width: 32%;"/>
+  <img src="images/step3.png" style="width: 32%;"/>
+</div>
+
 ## Setup
-* Required dependencies
-    * [Docker](https://docs.docker.com/engine/install/)
-    * [Docker compose](https://docs.docker.com/compose/install/)
-    * [Nvidia container toolkit](https://docs.nvidia.com/datacenter/cloud-native/container-toolkit/latest/install-guide.html)
+### Required dependencies
+* [Docker](https://docs.docker.com/engine/install/)
+* [Docker compose](https://docs.docker.com/compose/install/)
+* [Nvidia container toolkit](https://docs.nvidia.com/datacenter/cloud-native/container-toolkit/latest/install-guide.html)
+### Clone the repo
+``` bash
+git clone https://github.com/MrLaki5/QR-AI-generator.git
+git lfs install
+git submodule update --init --recursive
+```
+### Start the application
+```
+docker compose up -d
+```
+### Open starting page in browser under link [localhost:8089](localhost:8089)
 
-* Clone the repo
-    ``` bash
-    git clone https://github.com/MrLaki5/QR-AI-generator.git
-    git lfs install
-    git submodule update --init --recursive
-    ```
+## Deployment
+The project includes a deployment file specifically designed for deployment on the [Akash Network](https://akash.network/). All the latest docker images used in deployment file can be found on links [worker](https://hub.docker.com/repository/docker/mrlaki5/qr-ai-generator-worker/general) and [server](https://hub.docker.com/repository/docker/mrlaki5/qr-ai-generator-server/general).
 
-* Start the application
-    ```
-    docker compose up -d
-    ```
-
-* Open starting page in browser under link *localhost:8089*
+## References
+### AI Models
+This project utilizes open-source AI models.
+* [stable_diffusion_v1_5](https://huggingface.co/runwayml/stable-diffusion-v1-5)
+* [controlnet_v1p_sd15](https://huggingface.co/DionTimmer/controlnet_qrcode-control_v1p_sd15)
--- connection/redis_connector.py
+++ connection/redis_connector.py
@@ -1,4 +1,5 @@
 from redis import Redis, ConnectionPool
+import time
 
 
 class RedisConnector:
@@ -13,26 +14,66 @@ class RedisConnector:
 
     # Queue operations
     def push_in_queue(self, queue_name, value):
-        return self.redis.rpush(queue_name, value)
+        while True:
+            try:
+                return self.redis.rpush(queue_name, value)
+            except Exception as e:
+                print("RedisConnector.push_in_queue() error: ", e)
+                time.sleep(1)
 
     def pop_from_queue(self, queue_name):
-        return self.redis.blpop(queue_name)
+        while True:
+            try:
+                return self.redis.blpop(queue_name)
+            except Exception as e:
+                print("RedisConnector.pop_from_queue() error: ", e)
+                time.sleep(1)
 
     def get_queue_length(self, queue_name):
-        return self.redis.llen(queue_name)
+        while True:
+            try:
+                return self.redis.llen(queue_name)
+            except Exception as e:
+                print("RedisConnector.get_queue_length() error: ", e)
+                time.sleep(1)
 
     def get_all_from_queue(self, queue_name):
-        return self.redis.lrange(queue_name, 0, -1)
+        while True:
+            try:
+                return self.redis.lrange(queue_name, 0, -1)
+            except Exception as e:
+                print("RedisConnector.get_all_from_queue() error: ", e)
+                time.sleep(1)
 
     # Set operations
     def put_in_set(self, hash_name, key, value):
-        return self.redis.hset(hash_name, key, value)
+        while True:
+            try:
+                return self.redis.hset(hash_name, key, value)
+            except Exception as e:
+                print("RedisConnector.put_in_set() error: ", e)
+                time.sleep(1)
 
     def check_in_set(self, hash_name, key):
-        return self.redis.hexists(hash_name, key)
+        while True:
+            try:
+                return self.redis.hexists(hash_name, key)
+            except Exception as e:
+                print("RedisConnector.check_in_set() error: ", e)
+                time.sleep(1)
 
     def get_from_set(self, hash_name, key):
-        return self.redis.hget(hash_name, key)
+        while True:
+            try:
+                return self.redis.hget(hash_name, key)
+            except Exception as e:
+                print("RedisConnector.get_from_set() error: ", e)
+                time.sleep(1)
 
     def remove_from_set(self, hash_name, key):
-        return self.redis.hdel(hash_name, key)
+        while True:
+            try:
+                return self.redis.hdel(hash_name, key)
+            except Exception as e:
+                print("RedisConnector.remove_from_set() error: ", e)
+                time.sleep(1)
